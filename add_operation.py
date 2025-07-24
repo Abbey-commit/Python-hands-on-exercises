1858d45fdf39beda6fe2822f1f129d04b6920bd4
adebisi abiodun <adebisiabiodun614@gmail.com>
2025-07-24 21:05:37 +0100

    Added works on PYTHON functions techniques, control statements and data structure tooling techniques in Python

---
+++ add_operation.py
@@ -0,0 +1,10 @@
+import math_operation
+
+result1 = math_operation.add(4, 5)
+
+print(result1)
+
+from math_operation import add
+
+result2 = add(4, 5)
+print(result2)
---
+++ math_operation.py
@@ -0,0 +1,5 @@
+def math(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
